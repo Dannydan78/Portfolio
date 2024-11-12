4c4e549f783d8f224200bb2dd2feafd3ee127926
Danny <dannydan@MBP-de-Danny.lan>
2024-11-12 01:24:01 +0100

    slid change

---
+++ components/assets/icons/Jsai.vue
@@ -0,0 +1,9 @@
+<template>
+     <svg fill="#42b883" viewBox="-0.96 -0.96 25.92 25.92" role="img" xmlns="http://www.w3.org/2000/svg" stroke="#42b883" transform="matrix(1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Vue.js icon</title><path d="M24,1.61H14.06L12,5.16,9.94,1.61H0L12,22.39ZM12,14.08,5.16,2.23H9.59L12,6.41l2.41-4.18h4.43Z"></path></g></svg>
+</template>
+
+<style scoped>
+svg{
+    z-index: 1000;
+}
+</style>
